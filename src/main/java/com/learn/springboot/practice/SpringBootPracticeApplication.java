b95a21a8bc9c5f5c894fddb031e052aa120efd1f
lifeiqiang <Fqli12071@163.com>
2021-07-05 19:07:22 +0800

    Spring-Retry

--- src/main/java/com/learn/springboot/practice/SpringBootPracticeApplication.java
+++ src/main/java/com/learn/springboot/practice/SpringBootPracticeApplication.java
@@ -14,8 +14,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @Slf4j
 public class SpringBootPracticeApplication {
     public static void main(String[] args) {
-        log.info("SpringBootPracticeApplication start");
         SpringApplication.run(SpringBootPracticeApplication.class, args);
-        log.info("SpringBootPracticeApplication finish");
     }
 }
