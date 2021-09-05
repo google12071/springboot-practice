d4aafd3106247aec0225798adce49413d6b00875
lifeiqiang <Fqli12071@163.com>
2021-09-05 22:00:14 +0800

    Spring异步线程池

--- src/test/java/com/learn/springboot/practice/async/AsyncTaskTest.java
+++ src/test/java/com/learn/springboot/practice/async/AsyncTaskTest.java
@@ -6,13 +6,26 @@ import org.junit.Test;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.annotation.Resource;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Spring异步执行任务
  */
 @Slf4j
 public class AsyncTaskTest extends BaseTest {
+
+    /**
+     * 注入执行器
+     */
+    @Resource(name = "myExecutor")
+    private ThreadPoolTaskExecutor executor;
+
+    /**
+     * 演示需要，用于控制任务全部执行完毕后关闭线程池
+     */
     private static final CountDownLatch latch = new CountDownLatch(8);
 
     /**
@@ -41,18 +54,23 @@ public class AsyncTaskTest extends BaseTest {
         }
     }
 
-    /**
-     * 注入执行器
-     */
-    @Resource(name = "myExecutor")
-    private ThreadPoolTaskExecutor executor;
 
     @Test
-    public void printMessage() throws InterruptedException {
+    public void printMessage() throws InterruptedException, ExecutionException {
         for (int i = 0; i < 8; i++) {
             executor.execute(new AsyncTask("message:" + i));
         }
+        //处理有返回值的异步任务
+        Future<Object> future = executor.submit(new Callable<Object>() {
+            @Override
+            public String call() throws Exception {
+                log.info("Thread:{},invoke", Thread.currentThread().getName());
+                return "hello";
+            }
+        });
+        log.info("异步返回value:{}", future.get());
         latch.await();
+        //任务全部执行完成后关闭线程池
         executor.shutdown();
     }
 }
