816d66d468e00421b9567ef1041154be327ff0bf
lifeiqiang <Fqli12071@163.com>
2022-01-19 11:48:23 +0800

    prometheus监控

---
+++ src/main/java/com/learn/springboot/practice/config/metric/MetricConfig.java
@@ -0,0 +1,17 @@
+package com.learn.springboot.practice.config.metric;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MetricConfig {
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+
+}
