f692b09621832e705e14e0bcbe0b0ec6a938dc93
feiqiangli <feiqiangli@91jkys.com>
2022-03-28 10:23:29 +0800

    线程操作测试类

---
+++ src/test/java/com/learn/springboot/practice/juc/ThreadTest.java
@@ -0,0 +1,76 @@
+package com.learn.springboot.practice.juc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程生命周期测试
+ */
+@Slf4j
+public class ThreadTest {
+    /**
+     * 线程等待
+     */
+    public static class WaitingTime implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                waitSecond(200);
+            }
+        }
+
+        //线程等待多少秒
+        public static final void waitSecond(long seconds) {
+            try {
+                TimeUnit.SECONDS.sleep(seconds);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 线程状态
+     */
+    public static class WaitingState implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (WaitingState.class) {
+                    try {
+                        //无超时等待
+                        WaitingState.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 线程阻塞
+     */
+    public static class BlockedThread implements Runnable {
+        @Override
+        public void run() {
+            synchronized (BlockedThread.class) {
+                while (true) {
+                    WaitingTime.waitSecond(100);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new Thread(new WaitingTime(), "WaitingTimeThread").start();
+        new Thread(new WaitingState(), "WaitingStateThread").start();
+
+        //BlockedThread-01线程会抢到锁，BlockedThread-02线程会阻塞
+        new Thread(new BlockedThread(), "BlockedThread-01").start();
+        new Thread(new BlockedThread(), "BlockedThread-02").start();
+    }
+}
